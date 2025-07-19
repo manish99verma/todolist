74db052a0786c95aa781b667d53a1b184ce45a67
Manish Kumar Verma <manish6203993@gmail.com>
2025-07-19 11:18:36 +0530

    complete

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TodoList from "./components/TodoList/TodoList";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,43 @@
+import { Component } from "react";
+
+import "./TodoList.css";
+
+class TodoList extends Component {
+  constructor() {
+    super();
+    this.state = { newTask: "", list: [] };
+  }
+
+  addToList = () => {
+    if (!this.state.newTask) return;
+
+    this.setState({
+      list: [this.state.newTask, ...this.state.list],
+      newTask: "",
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-container">
+        <div>
+          <input
+            onChange={(e) =>
+              this.setState({ ...this.state, newTask: e.target.value })
+            }
+            type="text"
+            value={this.state.newTask}
+          />
+          <button onClick={this.addToList}>Add</button>
+        </div>
+        <ul>
+          {this.state.list.map((curr) => (
+            <li key={curr}>{curr}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
